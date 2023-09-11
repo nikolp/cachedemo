8efaa650bce1db1cbf8fa0e14b9d22b67840dabb
Philip Nikolov <philip.k.nikolov@gmail.com>
2023-09-11 11:30:24 -0400

    example logging

--- src/main/java/com/example/cachedemo/MainController.java
+++ src/main/java/com/example/cachedemo/MainController.java
@@ -1,15 +1,18 @@
 package com.example.cachedemo;
 
 import com.example.cachedemo.model.Book;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class MainController {
 
     @GetMapping("/book/{id}")
     Book getBook(@PathVariable int id) {
+        log.info("Expensive call for " + id);
         Book book = new Book();
         book.setId(id);
         book.setTitle("Book" + id);
