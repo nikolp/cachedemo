3d1df63e9e182b1ef4e50d9f68284885897fc8e7
Philip Nikolov <philip.k.nikolov@gmail.com>
2023-09-12 10:21:23 -0400

    redis config

---
+++ src/main/java/com/example/cachedemo/config/ApplicationConfiguration.java
@@ -0,0 +1,31 @@
+package com.example.cachedemo.config;
+
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.cache.RedisCacheManagerBuilderCustomizer;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+import java.time.Duration;
+
+@SpringBootConfiguration
+@EnableCaching
+public class ApplicationConfiguration {
+    @Bean
+    public RedisCacheConfiguration cacheConfiguration() {
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(60))
+                .disableCachingNullValues()
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(
+                        new GenericJackson2JsonRedisSerializer()));
+    }
+
+    @Bean
+    public RedisCacheManagerBuilderCustomizer redisCacheManagerBuilderCustomizer() {
+        return (builder) -> builder
+                .withCacheConfiguration("bookCache",
+                        cacheConfiguration().entryTtl(Duration.ofSeconds(20)));
+      }
+}
