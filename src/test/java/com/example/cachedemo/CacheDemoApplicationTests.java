61ec8f338c18d3c270749498d2d0aca2766c1a28
Philip Nikolov <philip.k.nikolov@gmail.com>
2023-09-13 10:31:06 -0400

    annotate the test as local runs

--- src/test/java/com/example/cachedemo/CacheDemoApplicationTests.java
+++ src/test/java/com/example/cachedemo/CacheDemoApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.cachedemo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("idea")
 class CacheDemoApplicationTests {
 
 	@Test
