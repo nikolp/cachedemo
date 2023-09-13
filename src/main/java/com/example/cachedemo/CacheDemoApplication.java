2a7dcfa5177b1dababcbf0485fcf779d600531ee
Philip Nikolov <philip.k.nikolov@gmail.com>
2023-09-13 10:30:37 -0400

    reorganized code

--- src/main/java/com/example/cachedemo/CacheDemoApplication.java
+++ src/main/java/com/example/cachedemo/CacheDemoApplication.java
@@ -1,13 +1,25 @@
 package com.example.cachedemo;
 
+import com.example.cachedemo.model.CacheUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+@Slf4j
 @SpringBootApplication
 public class CacheDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CacheDemoApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner demo(CacheUtil cacheUtil) {
+		return args -> {
+			cacheUtil.clearAll();
+		};
 	}
 
 }
