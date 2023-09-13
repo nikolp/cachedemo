87989a2f6d826e178315de0399b84c745280268b
Philip Nikolov <philip.k.nikolov@gmail.com>
2023-09-13 10:31:21 -0400

    caching helpers

---
+++ src/main/java/com/example/cachedemo/model/CacheUtil.java
@@ -0,0 +1,26 @@
+package com.example.cachedemo.model;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
+import org.springframework.data.redis.cache.RedisCache;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+
+@Slf4j
+@Service
+public class CacheUtil {
+    CacheUtil(CacheManager cacheManager) {this.cacheManager = cacheManager;}
+    private CacheManager cacheManager;
+    public void clearAll() {
+        for (String cacheName : cacheManager.getCacheNames()) {
+            Cache cache = cacheManager.getCache(cacheName);
+            RedisCache redisCache = (RedisCache) cache;
+            Duration expiry = redisCache.getCacheConfiguration().getTtl();
+            log.info("Examining cache: " + cacheName + " with expiry " + expiry);
+            cache.clear();
+            log.info("Cleared it");
+        }
+    }
+}
