e0081ee010392ef83abb7446251236c319425401
Philip Nikolov <philip.k.nikolov@gmail.com>
2023-09-19 14:54:45 -0400

    handle the "simple" HashMap based cache provider

--- src/main/java/com/example/cachedemo/model/CacheUtil.java
+++ src/main/java/com/example/cachedemo/model/CacheUtil.java
@@ -3,6 +3,7 @@ package com.example.cachedemo.model;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
+import org.springframework.cache.concurrent.ConcurrentMapCache;
 import org.springframework.data.redis.cache.RedisCache;
 import org.springframework.stereotype.Service;
 
@@ -16,12 +17,22 @@ public class CacheUtil {
     public void clearAll() {
         for (String cacheName : cacheManager.getCacheNames()) {
             Cache cache = cacheManager.getCache(cacheName);
-            RedisCache redisCache = (RedisCache) cache;
-            Duration expiry = redisCache.getCacheConfiguration().getTtl();
-            log.info("Examining cache: " + cacheName + " with expiry " + expiry);
-            redisCache.clear();
-            redisCache.clearStatistics();
-            log.info("Cleared it");
+            if (cache instanceof RedisCache) {
+                RedisCache redisCache = (RedisCache) cache;
+                Duration expiry = redisCache.getCacheConfiguration().getTtl();
+                log.info("Examining Redis cache: " + cacheName + " with expiry " + expiry);
+                redisCache.clear();
+                redisCache.clearStatistics();
+                log.info("Cleared it");
+            }
+            else if (cache instanceof ConcurrentMapCache) {
+                ConcurrentMapCache mapCache = (ConcurrentMapCache) cache;
+                log.info("Examining ConcurrentMapCache cache: {}", cacheName);
+                mapCache.clear();
+                log.info("Cleared it");
+            } else {
+                throw new RuntimeException("Do not know how to handle " + cache);
+            }
         }
     }
 }
---
+++ src/test/java/com/example/cachedemo/BookServiceImplConcurrentMapTest.java
@@ -0,0 +1,51 @@
+package com.example.cachedemo;
+
+import com.example.cachedemo.model.CacheUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.concurrent.ConcurrentMapCache;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
+
+@Slf4j
+// Same config as BookServiceImplTest
+// Except for the property override below
+// Trying to see that despite all Redis availability,
+// the finally chosen implementation is in-memory simple HashMap
+@SpringBootTest(properties = "spring.cache.type=simple")
+@ActiveProfiles("local")
+@TestMethodOrder(MethodOrderer.Random.class)  // ensure sequential order since shared cache
+@Import(TestConfigurationEmbeddedRedis.class)
+class BookServiceImplConcurrentMapTest {
+    @Autowired
+    BookServiceImpl bookService;
+    @Autowired
+    CacheManager cacheManager;
+    @Autowired
+    CacheUtil cacheUtil;
+
+    @BeforeEach
+    void SetUp() throws InterruptedException {
+        cacheUtil.clearAll();
+    }
+
+    @Test
+    void whenCallingFromOutside_UserConcurrentHashMap() {
+        log.info("This should appear once:");
+        bookService.getBook("1");
+        bookService.getBook("1");
+        Cache cache = cacheManager.getCache("book-cache");
+        ConcurrentMapCache mapCache = (ConcurrentMapCache) cache;
+        assertThat(mapCache.getNativeCache().keySet(), contains("1"));
+    }
+}
